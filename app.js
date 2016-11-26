e131e5b004d705da9df68d9b1a76aaa8eaf8a3f1
Erik Noorland <mail@eriknoorland.nl>
2016-11-26 20:48:30 +0100

    Add status LED

--- app.js
+++ app.js
@@ -3,10 +3,16 @@
 var express = require('express');
 var app = express();
 
+var Gpio = require('onoff').Gpio;
+var led = new Gpio(23, 'out');
+
 // serve static files on index for the connected client
 app.use('/', express.static(__dirname + '/static'));
 
 var leds = require('./routes/leds');
 app.use('/leds', leds);
+
+// turn status LED on
+led.writeSync(1);
 
 module.exports = app;
